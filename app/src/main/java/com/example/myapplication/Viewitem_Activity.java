c9d46470f37890a358926f283d993261296f1694
my630 <448151119@qq.com>
2020-06-30 20:59:50 +0800

    630_1

--- app/src/main/java/com/example/myapplication/Viewitem_Activity.java
+++ app/src/main/java/com/example/myapplication/Viewitem_Activity.java
@@ -1,15 +1,27 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
 
-public class Viewitem_Activity extends FragmentActivity {
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.myapplication.dummy.DummyContent;
+
+public class Viewitem_Activity extends FragmentActivity implements ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewitem_);
+        //1.获取framgmentMananger 2.获取transaction
+        //3.用rep;ace方法,替换原来占位置用的FragmentLayout控件
+        //4.commit
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frameLayout,ItemFragment.newInstance(1))
+                .commit();
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }

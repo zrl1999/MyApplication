420e865853256bdec7acff6bad7871faa6eb880d
my630 <448151119@qq.com>
2020-06-30 20:33:42 +0800

    630

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,48 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextname;
+    EditText editTextpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextname = findViewById(R.id.editText);
+        editTextpass = findViewById(R.id.editText2);
     }
 
-//    public void testgetRequest(View view) throws ExecutionException, InterruptedException {
-//        System.out.println(OkHttpUtil.getRequest("https://www.baidu.com"));
-//    }
+    public void testgetRequest(View view) throws ExecutionException, InterruptedException {
+        System.out.println(OkHttpUtil.getRequest("https://www.baidu.com"));
+    }
+
+    public void login(View view) throws ExecutionException, InterruptedException {
+        String name = editTextname.getText().toString();
+        String pass = editTextpass.getText().toString();
+        Map<String,String> map = new HashMap<>();
+        map.put("username",name);
+        map.put("userpass",pass);
+        String url = "http://172.18.85.254:8080/auction/api/users/login";
+        String str = OkHttpUtil.postRequest(url,map);
+        if (str.equals("1")){
+            Intent intent = new Intent(this,IndexActivity.class);
+            Toast.makeText(this,"登录成功",Toast.LENGTH_LONG).show();
+            startActivity(intent);
+
+        }else {
+            Toast.makeText(this,"登录失败",Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
--- app/src/main/java/com/example/myapplication/OkHttpUtil.java
+++ app/src/main/java/com/example/myapplication/OkHttpUtil.java
@@ -1,41 +1,60 @@
-//package com.example.myapplication;
-//
-//import java.util.Map;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.ExecutionException;
-//import java.util.concurrent.Executor;
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.FutureTask;
-//
-//import okhttp3.OkHttpClient;
-//import okhttp3.Request;
-//import okhttp3.Response;
-//
-//public class OkHttpUtil {
-//
-//    static OkHttpClient okHttpClient = new OkHttpClient();
-//    static ExecutorService threadPool = Executors.newFixedThreadPool(30);
-//
-//    public static String getRequest(final String url) throws ExecutionException, InterruptedException {
-//        FutureTask futureTask =new FutureTask(new Callable() {
-//            @Override
-//            public Object call() throws Exception {
-//                Request request = new Request.Builder()
-//                        .url(url)
-//                        .build();
-//                Response response = okHttpClient.newCall(request).execute();
-//                if (response.isSuccessful()&&response.body()!=null){
-//                    return response.body().string();
-//                }else {
-//                    return null;
-//                }
-//            }
-//        }) ;
-//        threadPool.execute(futureTask);
-//        return futureTask.get().toString();
-//    }
-//    public static String postRequest(String url, Map<String,String>map){
-//        return null;
-//    }
-//}
+package com.example.myapplication;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkHttpUtil {
+    static OkHttpClient okHttpClient = new OkHttpClient();
+    //创建一个线程池
+    static ExecutorService threadpool = Executors.newFixedThreadPool(30);
+    public static String getRequest(final String url) throws ExecutionException, InterruptedException {
+        FutureTask futureTask = new FutureTask(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                //创建Request对象
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                //获取服务器返回的数据
+                Response response = okHttpClient.newCall(request).execute();
+                if (response.isSuccessful()&&response.body()!=null){
+                    return response.body().string();
+                }else {
+                    return null; } }});
+        //把futureTask放到线程池中去执行
+        threadpool.execute(futureTask);
+        return futureTask.get().toString();
+    }
+    public static String postRequest(final String url, final Map<String,String> map) throws ExecutionException, InterruptedException {
+        FutureTask futureTask = new FutureTask(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                FormBody.Builder builder = new FormBody.Builder();
+                map.forEach(builder::add);
+                FormBody body = builder.build();
+
+                //创建Request对象
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+                //获取服务器返回的数据
+                Response response = okHttpClient.newCall(request).execute();
+                if (response.isSuccessful()&&response.body()!=null){
+                    return response.body().string();
+                }else {
+                    return null; } }});
+        //把futureTask放到线程池中去执行
+        threadpool.execute(futureTask);
+        return futureTask.get().toString();
+    }
+}
